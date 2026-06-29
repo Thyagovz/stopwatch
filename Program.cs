7a6616438468a6c6e6839944b5bfc01d70839e40
Thyago Euclides <flooddk@gmail.com>
2026-06-29 10:48:46 -0400

    Refactor stopwatch functionality by adding a menu and updating the Start method signature

--- Program.cs
+++ Program.cs
@@ -4,17 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        Start(6);
     }
 
-    static void Start()
+    static void Menu()
     {
-        int time = 10;
+        Console.Clear();
+        Console.WriteLine("S = Segundo");
+        Console.WriteLine("M = Minuto");
+        Console.WriteLine("0 = Sair");
+        Console.WriteLine("Quanto tempo deseja contar?");
+    }
+
+    static void Start(int time)
+    {
         int currentTime = 0;
 
-        while(currentTime != time)
+        while (currentTime != time)
         {
+            Console.Clear();
             currentTime++;
+            Console.WriteLine(currentTime);
+            Thread.Sleep(1000);
         }
+
+        Console.Clear();
+        Console.WriteLine("Stopwatch finalizado");
+        Thread.Sleep(2500);
     }
 }
