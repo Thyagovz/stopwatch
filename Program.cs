7ebe638ac84019e2fcb9e9959f564fffaebd087a
Thyago Euclides <flooddk@gmail.com>
2026-06-29 10:38:39 -0400

    Add initial project files including .gitignore, Program.cs, and stopwatch.csproj

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Stopwatch;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+
+    static void Start()
+    {
+        int time = 10;
+        int currentTime = 0;
+
+        while(currentTime != time)
+        {
+            currentTime++;
+        }
+    }
+}
