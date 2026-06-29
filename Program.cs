7ac38c483fa99212ffabee0e28aeb93a2e2fa3a1
Thyago Euclides <flooddk@gmail.com>
2026-06-29 11:00:35 -0400

    Update Menu method to enhance user instructions and comment out Start method call

--- Program.cs
+++ Program.cs
@@ -4,18 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Start(6);
+        // Start(6);
+        Menu();
     }
 
     static void Menu()
     {
         Console.Clear();
-        Console.WriteLine("S = Segundo");
-        Console.WriteLine("M = Minuto");
+        Console.WriteLine("S = Segundo => 10s = 10 segundos");
+        Console.WriteLine("M = Minuto => 1m = 1 minuto");
         Console.WriteLine("0 = Sair");
         Console.WriteLine("Quanto tempo deseja contar?");
-    }
 
+        string data = Console.ReadLine().ToLower();
+        char type = char.Parse(data.Substring(data.Length - 1, 1));
+        Console.WriteLine(data);
+        Console.WriteLine(type);
+    }
     static void Start(int time)
     {
         int currentTime = 0;
