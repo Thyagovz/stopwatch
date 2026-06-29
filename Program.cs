0bd329da9d3ee2361ccbcd751874aaef30ebeea1
Thyago Euclides <flooddk@gmail.com>
2026-06-29 11:10:08 -0400

    Refactor Menu method to improve time parsing and exit logic

--- Program.cs
+++ Program.cs
@@ -19,9 +19,15 @@ class Program
         string data = Console.ReadLine().ToLower();
         char type = char.Parse(data.Substring(data.Length - 1, 1));
         int time = int.Parse(data.Substring(0, data.Length - 1));
+        int multiplier = 1;
 
-        Console.WriteLine(type);
-        Console.WriteLine(time);
+        if (type == 'm')
+            multiplier = 60;
+
+        if (time == 0)
+            System.Environment.Exit(0);
+
+        Start(time * multiplier);
     }
     static void Start(int time)
     {
@@ -38,5 +44,6 @@ class Program
         Console.Clear();
         Console.WriteLine("Stopwatch finalizado");
         Thread.Sleep(2500);
+        Menu();
     }
 }
